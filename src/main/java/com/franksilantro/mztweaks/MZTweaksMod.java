12d9f603216709bb6d1f5152fa77d370090d34a4
thejackbitt <jack.bittner@outlook.com>
2024-09-11 15:21:47 -0500

    new starter content

--- src/main/java/com/franksilantro/mztweaks/MZTweaksMod.java
+++ src/main/java/com/franksilantro/mztweaks/MZTweaksMod.java
@@ -1,14 +1,14 @@
-package com.example.examplemod;
+package com.franksilantro.mztweaks;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MZTweaksMod.MODID, version = MZTweaksMod.VERSION)
+public class MZTweaksMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "mztweaks";
     public static final String VERSION = "1.0";
 
     @EventHandler
