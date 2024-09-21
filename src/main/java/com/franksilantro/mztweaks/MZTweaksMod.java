e81911a6f710095b5e3775a5708d6d827b30779f
thejackbitt <jack.bittner@outlook.com>
2024-09-20 22:02:29 -0500

    swords and cool stuff

--- src/main/java/com/franksilantro/mztweaks/MZTweaksMod.java
+++ src/main/java/com/franksilantro/mztweaks/MZTweaksMod.java
@@ -2,8 +2,11 @@ package com.franksilantro.mztweaks;
 
 import com.franksilantro.mztweaks.shield.MZShield;
 import com.franksilantro.mztweaks.shield.MZShieldItems;
-
+import com.franksilantro.mztweaks.weapon.MZSwordType;
 import com.franksilantro.mztweaks.client.renderer.MZShieldRenderer;
+import com.franksilantro.mztweaks.client.renderer.MZSwordRenderer;
+import com.franksilantro.mztweaks.events.KillstreakEventHandler;
+import lotr.client.render.item.LOTRRenderLargeItem;
 
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
@@ -11,11 +14,15 @@ import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
-import cpw.mods.fml.common.eventhandler.EventBus;
+
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
+import net.minecraft.item.Item.ToolMaterial;
+import net.minecraftforge.common.util.EnumHelper;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraft.client.audio.SoundHandler;
+import net.minecraft.client.Minecraft;
 
 @Mod(modid = MZTweaksMod.MODID, version = MZTweaksMod.VERSION)
 public class MZTweaksMod {
@@ -61,7 +68,12 @@ public class MZTweaksMod {
     public static Item umbarShield;
     public static Item urukShield;
     public static Item woodelfShield;
-
+    // Swords
+    public static Item bardsbane;
+    public static Item braveheart;
+    public static Item crucible;
+    public static Item mastersword;
+    public static Item warhammer40k;
 
     @EventHandler
 public void preInit(FMLPreInitializationEvent event) {
@@ -109,6 +121,19 @@ public void preInit(FMLPreInitializationEvent event) {
     umbarShield = new MZShieldItems(MZShield.UMBAR).setUnlocalizedName("umbar_shield");
     urukShield = new MZShieldItems(MZShield.URUK).setUnlocalizedName("uruk_shield");
     woodelfShield = new MZShieldItems(MZShield.WOODELF).setUnlocalizedName("woodelf_shield");
+
+    // Swords!!!!
+    bardsbane = new MZSwordType().setUnlocalizedName("bardsbane").setFull3D().setTextureName("mztweaks:bardsbane");
+    GameRegistry.registerItem(bardsbane, MODID + "_bardsbane");
+    braveheart = new MZSwordType().setUnlocalizedName("braveheart").setFull3D().setTextureName("mztweaks:braveheart");
+    GameRegistry.registerItem(braveheart, MODID + "_braveheart");
+    crucible = new MZSwordType().setUnlocalizedName("crucible").setFull3D().setTextureName("mztweaks:crucible");
+    GameRegistry.registerItem(crucible, MODID + "_crucible");
+    mastersword = new MZSwordType().setUnlocalizedName("mastersword").setFull3D().setTextureName("mztweaks:mastersword");
+    GameRegistry.registerItem(mastersword, MODID + "_mastersword");
+    warhammer40k = new MZSwordType().setUnlocalizedName("warhammer40k").setFull3D().setTextureName("mztweaks:warhammer40k");
+    GameRegistry.registerItem(warhammer40k, MODID + "_warhammer40k");
+
 }
 
 
@@ -157,6 +182,15 @@ public void preInit(FMLPreInitializationEvent event) {
     MinecraftForgeClient.registerItemRenderer((MZShieldItems) umbarShield, new MZShieldRenderer());
     MinecraftForgeClient.registerItemRenderer((MZShieldItems) urukShield, new MZShieldRenderer());
     MinecraftForgeClient.registerItemRenderer((MZShieldItems) woodelfShield, new MZShieldRenderer());
-    }
 
+    MinecraftForgeClient.registerItemRenderer(bardsbane, new MZSwordRenderer());
+    MinecraftForgeClient.registerItemRenderer(braveheart, new MZSwordRenderer());
+    MinecraftForgeClient.registerItemRenderer(crucible, new MZSwordRenderer());
+    MinecraftForgeClient.registerItemRenderer(mastersword, new MZSwordRenderer());
+    MinecraftForgeClient.registerItemRenderer(warhammer40k, new MZSwordRenderer());
+
+    MinecraftForge.EVENT_BUS.register(new KillstreakEventHandler(new MZSwordType()));
+
+    SoundHandler soundHandler = Minecraft.getMinecraft().getSoundHandler();
+    }
 }
